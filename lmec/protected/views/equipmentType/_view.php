a8f15a71c5c5a98230fd9373b8ec193a7c1e98fd
said <black.dawn.90@gmail.com>
2013-04-20 15:02:40 -0500

    eliminado

--- lmec/protected/views/equipmentType/_view.php
+++
@@ -1,7 +0,0 @@
-<div class="view">
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('type')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->type), array('view','id'=>$data->id)); ?>
-	<br />
-
-</div>
