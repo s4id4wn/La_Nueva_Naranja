ab195b1951af90b93005bca4655ea4e74e738f44
pako7 <fco.balam@hotmail.com>
2013-04-25 13:05:13 -0500

    base de datos (incompleto)
    archivos agregado: lanuevanaranja.sql

---
+++ Base de datos/lanuevanaranja.sql
@@ -0,0 +1,107 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+-- -----------------------------------------------------
+-- Table `tbl_electrodomestico`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_electrodomestico` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `nombre` VARCHAR(50) NOT NULL ,
+  `descripcion` VARCHAR(200) NULL ,
+  `precio` DECIMAL(7,2) NOT NULL ,
+  `activo` TINYINT(1) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_customer`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_customer` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(50) NOT NULL ,
+  `last_name` VARCHAR(50) NOT NULL ,
+  `active` TINYINT(1) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_venta`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_venta` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `fecha` DATETIME NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_historial_ventas`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_historial_ventas` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_role`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_role` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(50) NOT NULL ,
+  `priority` INT NOT NULL ,
+  `url_initial` VARCHAR(50) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_user`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_user` (
+  `id` INT UNSIGNED NULL AUTO_INCREMENT ,
+  `role_id` INT UNSIGNED NOT NULL ,
+  `user` VARCHAR(20) NOT NULL ,
+  `password` VARCHAR(35) NOT NULL ,
+  `name` VARCHAR(70) NOT NULL ,
+  `last_name` VARCHAR(45) NOT NULL ,
+  `telephone_number` VARCHAR(10) NOT NULL ,
+  `email` VARCHAR(45) NOT NULL ,
+  `address` VARCHAR(200) NOT NULL ,
+  `active` TINYINT(1) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_tbl_user_tbl_role_idx` (`role_id` ASC) ,
+  CONSTRAINT `fk_tbl_user_tbl_role`
+    FOREIGN KEY (`role_id` )
+    REFERENCES `tbl_role` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_` (
+)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tbl_lineablanca`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `tbl_lineablanca` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- JavaScript/user.js
+++ JavaScript/user.js
@@ -1,157 +1,261 @@
 
-function validateFormOfUser(user){
+function validateFormOfUser(User)
+{
 
-	user.name.value = user.name.value.trim();
+	var is_accepted = false;
 	
-	if(user.name.value=="")
-	{
-		$(document).ready( function(){
-		$('.error').show();
-		$('#error_name').show();
-		$('#error_name_short').hide();
-	});
-	
-	}else if(user.name.value!="")
-	{
-	
-		if(user.name.value.length < 3){
-			
-			$(document).ready( function(){
-			$('.error').show();
-			$('#error_name').hide();
-			$('#error_name_short').show();
-		});
-		
-		}else if (user.name.value.length >= 3){
-		
-		$(document).ready( function(){
-			$('.error').hide();
-			$('#error_name').hide();
-			$('#error_name_short').hide();
-		});
-		
-		}
-		
-	}
-	
-	user.last_name.value = user.last_name.value.trim();
-	
-	if(user.last_name.value==""){
-
-		$(document).ready(function(){
-		$('.error').show();
-			$('#error_last_name').show();
-		});
-	}else if(user.last_name.value!="")
-	{
-		$(document).ready( function(){
-		$('.error').hide();
-		$('#error_last_name').hide();
-		});
-	}
-
-	user.email.value = user.email.value.trim();
-	
-	if(user.email.value==""){
-
-		$(document).ready(function(){
-		$('.error').show();
-			$('#error_email').show();
-		});
-	}else if(user.email.value!="")
-	{
-		$(document).ready( function(){
-		$('.error').hide();
-		$('#error_email').hide();
-		});
-	}
-	
-	
-	user.repeat_email.value = user.repeat_email.value.trim();
-	
-	if(user.repeat_email.value==""){
-
-		$(document).ready(function(){
-		$('.error').show();
-			$('#error_repeat_email').show();
-		});
-	}else if(user.repeat_email.value!="")
-	{
-		$(document).ready( function(){
-		$('.error').hide();
-		$('#error_repeat_email').hide();
-		});
-	}
-	
-	
-
-
-
-	return false;
-}
-
-/*no utilizado*/
-function validateFormOfUser2( User ){
-
-	var is_accepted = true;
-	var message = "";
+	User.user.value = User.user.value.trim();
+	var user = User.user.value;
 	
 	User.name.value = User.name.value.trim();
+	var name = User.name.value;
+
 	User.last_name.value = User.last_name.value.trim();
+	var last_name = User.last_name.value;
+	
 	User.email.value = User.email.value.trim();
+	var email = User.email.value;
+	
 	User.repeat_email.value = User.repeat_email.value.trim();
+	var repeat_email = User.repeat_email.value;
+	
 	User.password.value = User.password.value.trim();
+	var password = User.password.value;
+	
 	User.confirm_password.value = User.confirm_password.value.trim();
+	var confirm_password = User.confirm_password.value;
+	
 	User.town.value = User.town.value.trim();
+	var town = User.town.value;
+	
+var a = this.validateUser(user);
+var b =	this.validateName(name);
+var c = this.validateLastName(last_name);
+var d = this.validateEmail(email);
+var e = this.validateRepeat_email(email,repeat_email);
+var f = this.validatePassword(password);
+var g = this.validateConfirmPassword(password,confirm_password);
+var h = this.validateTown(town);
+
+
+/*eliminar la sig linea*/
+return false;
+
+/* return is_accepted;*/
+}
+
+ function validateUser(user)
+ {
+	var is_accepted = false;
+	var minimum_length_user = 3;
+	var maxim_length_user = 20;
+	
+	if(user=="")
+	{
+		$('#error_user').show();
+		$('#error_u').text('Usuario, es requerido').show();
+	}else if(user!="")
+	{
+		if(user.length < minimum_length_user)
+		{
+			$('#error_user').show();
+			$('#error_u').text('No menor a 3 caracteres').show();		
+		}
+		else if (user.length > maxim_length_user)
+		{
+			$('#error_user').show();
+			$('#error_u').text('No mayor a 20 caracteres').show(); 
+		}
+		else
+		{
+			$('#error_user').hide();
+			$('#error_u').hide();
+			is_accepted=true;
+		}
+	}
+	return is_accepted;
+}
+
+function validateName(name)
+{
+	var is_accepted = false;
+	
+	if(name=="")
+	{
+		$('#error_name').show();
+		$('#error_n').text('Nombres, es requerido').show();
+	}
+	else if(name!="")
+	{
+		$('#error_name').hide();
+		$('#error_n').hide();
+		is_accepted=true;
+	}
+	return is_accepted;
+}
+
+
+function validateLastName(last_name)
+{
+	var is_accepted = false;
+	
+	if(last_name=="")
+	{
+		$('#error_last_name').show();
+		$('#error_ln').text('Apellidos, es requerido').show();
+	}
+	else if(last_name!="")
+	{
+		$('#error_last_name').hide();
+		$('#error_ln').hide();
+		is_accepted=true;
+	}
+	return is_accepted;
+}
+
+
+function validateEmail(email)
+{
+	var is_accepted = false;
+	
+	if(email=="")
+	{
+		$('#error_email').show();
+		$('#error_e').text('Correo es requerido').show();
+	}
+	else if(email!="")
+	{
+		var pattern_email=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+	
+		if (email.search(pattern_email)!=0)
+		{
+			$('#error_email').show();
+			$('#error_e').text('Correo inválido').show();;
+		}
+		else
+		{
+			$('#error_email').hide();
+			$('#error_e').hide();
+			is_accepted=true;
+		}
+	}
+	return is_accepted;
+}
+
+function validateRepeat_email(email,repeat_email)
+{
+	var is_accepted = false;
+
+	if(repeat_email=="")
+	{
+		$('#error_repeat_email').show();
+		$('#error_re').text('Repetir Correo es requerido').show();
+	}
+	else if(repeat_email!="")
+	{
+		var pattern_email=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+	
+		if (repeat_email.search(pattern_email)!=0)
+		{
+			$('#error_repeat_email').show();
+			$('#error_re').text('Correo inválido').show();
+		}
+		else
+		{
+			if(repeat_email!=email)
+			{
+				$('#error_repeat_email').show();
+				$('#error_re').text('Repetir correo exactamente').show();
+			}
+			else
+			{
+				$('#error_repeat_email').hide();
+				$('#error_re').hide();
+				is_accepted=true;
+			}
+		}
+	}
+	return is_accepted;
+}
+
+function validatePassword(password)
+{
+	var is_accepted = false;
+	var maxim_length_password = 35;
+	
+	if(password=="")
+	{
+		$('#error_password').show();
+		$('#error_p').text('Contraseña es requerido').show();
+	}
+	else if(password!="")
+	{
+		if (password.length > maxim_length_password)
+		{
+			$('#error_password').show();
+			$('#error_p').text('No mayor a 35 caracteres').show(); 
+		}
+		else
+		{
+			$('#error_password').hide();
+			$('#error_p').hide();
+			is_accepted=true;
+		}
+	}
+	return is_accepted;
+}
+
+function validateConfirmPassword(password,confirm_password)
+{
+	var is_accepted = false;
+	var maxim_length_confirm_password = 35;
+	
+	if(confirm_password=="")
+	{
+		$('#error_confirm_password').show();
+		$('#error_cp').text('Cofirmar Contraseña es requerido').show();
+	}
+	else if(confirm_password!="")
+	{
+		if (confirm_password.length > maxim_length_confirm_password)
+		{
+			$('#error_confirm_password').show();
+			$('#error_cp').text('No mayor a 35 caracteres').show(); 
+		}
+		else
+		{
+			if(password!=confirm_password)
+			{
+				$('#error_confirm_password').show();
+				$('#error_cp').text('Repetir contraseña exactamente').show();
+			}
+			else
+			{
+				$('#error_confirm_password').hide();
+				$('#error_cp').hide();
+				is_accepted=true;
+			}
+		}
+	}
+	return is_accepted ;
+}
+
+function validateTown(town){
+
+	var is_accepted = false;
+	
+
+	return is_accepted;
+}
+
+
+function a (){
+	
+	
+	
 	User.localidad.value = User.localidad.value.trim();
 	User.address.value = User.address.value.trim();
 	User.telephone_number.value = User.telephone_number.value.trim();
 
-	if( User.name.value=="" || User.name.value.length > 20 ){
-			message += "Campo nombre es requerido\n";
-			var n = document.getElementById("fco");	
-		n.innerHTML = "requerido";
-	
-			is_accepted = false;
-		}		
-	if( User.last_name.value=="" || User.last_name.value.length > 20 ){
-			message += "Campo apellido es requerido\n";
-			is_accepted = false;
-		}
-	if( User.email.value=="" || User.email.value.length > 20 ){
-			message += "Campo correo es requerido\n";
-			is_accepted = false;
-		}
-	if( User.repeat_email.value=="" || User.repeat_email.value.length > 20 ){
-		message += "Campo repetir correo es requerido\n";
-			is_accepted = false;
-		}
-	if( User.password.value=="" || User.password.value.length > 20 ){
-			message += "Campo contraseña es requerido\n";
-			is_accepted = false;
-		}
-	if( User.confirm_password.value=="" || User.confirm_password.value.length > 20 ){
-			message += "Campo confirmar contraseña es requerido\n";
-			is_accepted = false;
-		}
-	if( User.town.value=="" || User.town.value.length > 20 ){
-			message += "Campo municipio es requerido\n";
-			is_accepted = false;
-		}
-	if( User.localidad.value=="" || User.localidad.value.length > 20 ){
-			message += "Campo localidad es requerido\n";
-			is_accepted = false;
-		}
-	if( User.address.value=="" || User.address.value.length > 20 ){
-			message += "Campo dirección es requerido\n";
-			is_accepted = false;
-		}
-	if( User.telephone_number.value=="" || User.telephone_number.value.length > 20 ){
-			message += "Campo número telefonico es requerido\n";
-			is_accepted = false;
-		}
-		if(is_accepted==false){
-			
-		}
-	return is_accepted;
 }
+	
